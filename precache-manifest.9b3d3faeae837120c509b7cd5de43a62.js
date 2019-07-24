c305aaa47b2045a478c41c0e2b3dfe7b9ae90bcd
moshicode <moshikoshamir@live.com>
2019-07-24 23:10:34 +0300

    Updates

---
+++ precache-manifest.9b3d3faeae837120c509b7cd5de43a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0020edfc016f4e4764c192840abf0e17",
+    "url": "/raz/index.html"
+  },
+  {
+    "revision": "09adc0fec91c4efabe0b",
+    "url": "/raz/static/css/main.50ce63c8.chunk.css"
+  },
+  {
+    "revision": "d1e820bf8565937c28ba",
+    "url": "/raz/static/js/2.121a0f49.chunk.js"
+  },
+  {
+    "revision": "09adc0fec91c4efabe0b",
+    "url": "/raz/static/js/main.534fa804.chunk.js"
+  },
+  {
+    "revision": "5aba5c4b442a46c27f47",
+    "url": "/raz/static/js/runtime~main.c0385b98.js"
+  }
+]);
